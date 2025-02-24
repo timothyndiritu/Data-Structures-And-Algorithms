625fa84c9f6997d66da757f120a83401285a42a6
timothyndiritu <timmyndiritu@gmail.com>
2025-02-24 08:51:00 +0300

    factorial

--- factorial.js
+++ factorial.js
@@ -1,14 +1,30 @@
-
 //my solution
+// function factorial(n) {
+//   let factorial = 1;
+//   for (let i = 1; i < n + 1; i++){
+//     factorial = factorial * i;
+//   }
+//   return factorial
+// }
+
+// console.log(factorial(0)) // 1
+// console.log(factorial(1)) // 1
+// console.log(factorial(4)) // 24
+// console.log(factorial(5)) // 120
+
+//instructors solution
+
 function factorial(n) {
-  let factorial = 1;
-  for (let i = 1; i < n + 1; i++){
-    factorial = factorial * i;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
   }
-  return factorial
+  return result;
 }
 
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
 
-console.log(factorial(0)) // 1
-console.log(factorial(4)) // 24
-console.log(factorial(5)) // 120
+//Big -O is O(n)
